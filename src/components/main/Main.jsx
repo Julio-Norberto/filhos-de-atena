37f5189e6b52796e37f44add40d4c8bfc9642166
Júlio César <juliocsscl@gmail.com>
2022-05-08 22:06:17 -0300

    modify images content

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Contents from "./Contents";
 import './Main.css'
-import tech from '../../images/chip.png'
-import atom from '../../images/atom.png'
+import tech from '../../images/chip2.png'
+import atom from '../../images/atom2.png'
+import brain from '../../images/cherry-108.png'
 
 export default (props) => {
     return (
         <div className="Main">
             <Contents title="Tecnologia" img={tech} desc="Aprenda conceitos de diversas áreas da tecnologia como desenvolvimento front-end, back-end, hacking, redes e muito mais." />
             <Contents title="Ciências" img={atom} desc="Aprenda conceitos de diversas áreas da tecnologia como desenvolvimento front-end, back-end, hacking, redes e muito mais." />
-            <Contents title="Filosofia" img={atom} desc="Aprenda conceitos de diversas áreas da tecnologia como desenvolvimento front-end, back-end, hacking, redes e muito mais." />
+            <Contents title="Filosofia" img={brain} desc="Aprenda conceitos de diversas áreas da tecnologia como desenvolvimento front-end, back-end, hacking, redes e muito mais." />
         </div>
     )
 }
