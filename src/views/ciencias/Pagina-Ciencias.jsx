5dfea740f185e77f874f700a498ac079c6380361
Júlio César <juliocsscl@gmail.com>
2022-05-10 21:23:56 -0300

    create Pagina-Ciencias.jsx e Pagina-Filosofia.jsx

--- src/views/ciencias/Pagina-Ciencias.jsx
+++ src/views/ciencias/Pagina-Ciencias.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import DivContentTech from "../tech/Div-Content-Tech";
+import backgtech from '../../images/backgtehc.jpg'
+import './Pagina-Ciencias.css'
 
 export default (props) => {
     return (
-        <div>
-            <h1>Ciências</h1>
+        <div className="ciencias">
+            <div className="tech-topo">
+                <h1>Ciências</h1>
+                <p>Aprenda conceitos técnicos sobre áreas diversas da ciência moderna e antiga...</p>
+            </div>
+
+            <div className="component-content">
+                <DivContentTech title="Maldita maçã" image={backgtech} />
+                <DivContentTech title="Maldita maçã" image={backgtech} />
+                <DivContentTech title="Maldita maçã" image={backgtech} />
+                <DivContentTech title="Maldita maçã" image={backgtech} />
+            </div>
+
+            
         </div>
     )
 }
--- src/views/filosofia/Pagina-Filosofia.jsx
+++ src/views/filosofia/Pagina-Filosofia.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import DivContentTech from "../tech/Div-Content-Tech";
+import backgtech from '../../images/backgtehc.jpg'
+import './Pagina-Filosofia.css'
 
 export default (props) => {
     return (
-        <div>
-            <h1>Filosofia</h1>
+        <div className="filosofia">
+            <div className="tech-topo">
+                <h1>Filosofia</h1>
+                <p>Aprenda sobre as reflexões de grandes pensadores da antiguidade e da era atual...</p>
+            </div>
+
+            <div className="component-content">
+                <DivContentTech title="Porque choras socrates" image={backgtech} />
+                <DivContentTech title="Porque choras socrates" image={backgtech} />
+                <DivContentTech title="Porque choras socrates" image={backgtech} />
+                <DivContentTech title="Porque choras socrates" image={backgtech} />
+            </div>
+
+            
         </div>
     )
 }
