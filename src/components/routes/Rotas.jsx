4296f2d5ee46bbf57a4e3a50bb35a2da371ff287
Júlio César <juliocsscl@gmail.com>
2022-05-14 20:26:33 -0300

    create program-page.jsx and programacao-page.css

--- src/components/routes/Rotas.jsx
+++ src/components/routes/Rotas.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import PaginaCiencias from "../../views/ciencias/Pagina-Ciencias";
 import PaginaFilosofia from "../../views/filosofia/Pagina-Filosofia";
 import Tech from "../../views/tech/Pagina-Tech";
+import ProgramPage from "../../views/tech/programacao/program-page";
 import Home from "../home/Home";
 
 export default (props) => {
@@ -14,6 +15,7 @@ export default (props) => {
                 <Route path="/tech" element={<Tech/>} />
                 <Route path="/ciencias" element={<PaginaCiencias />} />
                 <Route path="/filosofia" element={<PaginaFilosofia />} />
+                <Route path="/tech/programacao" element={<ProgramPage />} />
 
             </Routes>
         </div>
--- src/views/tech/Pagina-Tech.jsx
+++ src/views/tech/Pagina-Tech.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DivContentTech from "./Div-Content-Tech";
 import './Pagina-Tech.css'
 import backgtech from '../../images/backgtehc.jpg'
+import { Link } from "react-router-dom";
 
 export default (props) => {
     return (
@@ -11,14 +12,27 @@ export default (props) => {
                 <p>Aprenda sobre redes de computadores, programação, hacking e muito mais...</p>
             </div>
 
-            <div className="component-content">
-                <DivContentTech title="Mamacos pelados" image={backgtech} />
-                <DivContentTech title="Mamacos pelados" image={backgtech} />
-                <DivContentTech title="Mamacos pelados" image={backgtech} />
-                <DivContentTech title="Mamacos pelados" image={backgtech} />
+            <div className="content-types">
+
+                <div className="types">
+                    <h1>Programação</h1>
+                    <p>Aprenda sobre HTML, CSS, JavaScript e diversos conceitos da programação web</p>
+                    <Link className="links" to={"/tech/programacao"}>Acessar...</Link>
+                </div>
+
+                <div className="types">
+                    <h1>Hacking</h1>
+                    <p>Aprenda sobre os pilares da segurança da informação e sobre ferramentas de pentest</p>
+                    <Link className="links" to={"/tech/programacao"}>Acessar...</Link>
+                </div>
+
+                <div className="types">
+                    <h1>Redes</h1>
+                    <p>Aprenda sobre os conceitos de redes de computadores, equipamentos e serviços</p>
+                    <Link className="links" to={"/tech/programacao"}>Acessar...</Link>
+                </div>
             </div>
 
-            
         </div>
     )
 }
